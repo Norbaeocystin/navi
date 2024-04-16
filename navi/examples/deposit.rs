6e18f0d2abf9ed93c94bec56cd00dae18ee9f9bd
Norbaeocystin <baranrastislav@gmail.com>
2024-04-16 12:46:55 -0400

    deposit example

--- navi/examples/deposit.rs
+++ navi/examples/deposit.rs
@@ -2,7 +2,7 @@ use log::{info, LevelFilter};
 use sui_sdk::SuiClientBuilder;
 use sui_types::base_types::SuiAddress;
 use sui_types::programmable_transaction_builder::ProgrammableTransactionBuilder;
-use sui_types::transaction::{Argument, TransactionKind};
+use sui_types::transaction::{Argument, CallArg, Command, TransactionKind};
 use misc_utils::transaction::TransactionWrapper;
 use misc_utils::utils::coin_value;
 use navi::navi::Navi;
@@ -16,8 +16,6 @@ async fn main() {
         .await.unwrap();
     // T0D0 change to your address
     let sui_address = SuiAddress::ZERO;
-    let BORROW = 1_000_000_000_000;
-    let FEE = 6_000_000_000;
     let navi = Navi::new(&client, sui_address).await;
     let mut tb = ProgrammableTransactionBuilder::new();
     let asset = "0x0000000000000000000000000000000000000000000000000000000000000002::sui::SUI";
@@ -26,33 +24,12 @@ async fn main() {
     let mut ct = asset.to_string();
     ct = ct.replace("0x","");
     // 0
-    tb = lending_flash_loan_with_ctx(tb,asset.parse().unwrap(), pool_id.clone(), isv.clone(), BORROW);
-    // 1
-    tb.programmable_move_call(
-        "0x0000000000000000000000000000000000000000000000000000000000000002".parse().unwrap(),
-        "coin".parse().unwrap(),
-        "from_balance".parse().unwrap(),
-        vec![asset.parse().unwrap()],
-        vec![Argument::NestedResult(0,0)],
-    );
-    // 2
-    tb = coin_value(tb, asset.parse().unwrap(), Argument::Result(1));
-    // 3
-    tb = navi.deposit(asset.parse().unwrap(), tb, Argument::Result(1), Argument::Result(2) );
-    // 4
-    tb = navi.borrow_as_balance(asset.parse().unwrap(), BORROW + FEE, tb);
-    // 5
-    tb = lending_flash_repay_with_ctx(tb, asset.parse().unwrap(), pool_id.clone(), isv.clone(), Argument::NestedResult(0, 1), Argument::Result(4));
-    // 6
-    tb.programmable_move_call(
-        "0x0000000000000000000000000000000000000000000000000000000000000002".parse().unwrap(),
-        "balance".parse().unwrap(),
-        "destroy_zero".parse().unwrap(),
-        vec![asset.parse().unwrap()],
-        vec![Argument::Result(5)],
-    );
-
+    let value = tb.input(CallArg::Pure(1_000_000_000_u64.to_le_bytes().to_vec())).unwrap();
+    tb.command(Command::SplitCoins(Argument::GasCoin, vec![value]));
+    tb = navi.deposit(asset.parse().unwrap(), tb, Argument::NestedResult(0,0), value);
     let ptb = tb.finish();
+    // let txwrapper = TransactionWrapper::new(&client);
+    // let result = txwrapper.process_ptx(ptb, None, None, None).await;
     let result = client.read_api().dev_inspect_transaction_block(sui_address, TransactionKind::ProgrammableTransaction(ptb.clone()), None, None, None).await.unwrap();
     info!("{:#?}", result);
 }
